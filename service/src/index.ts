b71632606fe0b6aad81afa514704f79a2a9487a5
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-07-13 12:49:27 -0600

    Add sequelize and dotenv

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,2 +1,13 @@
-import fs from 'fs';
-fs;
+import { Sequelize } from 'sequelize';
+import 'dotenv/config';
+
+const sequelize = new Sequelize(`postgres://${process.env.PG_USERNAME}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`);
+
+try {
+  await sequelize.authenticate();
+  console.log('Connection has been established successfully');
+  await sequelize.close();
+  console.log('Connection has been closed');
+} catch (error) {
+  console.log('Unable to connect to the database', error);
+}
