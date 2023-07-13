643bfc492d12bb5b942ac457830d688648af317d
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-07-13 16:28:36 -0600

    Remove sequelize example

--- service/src/index.ts
+++ service/src/index.ts
@@ -1,13 +1,2 @@
-import { Sequelize } from 'sequelize';
 import 'dotenv/config';
 
-const sequelize = new Sequelize(`postgres://${process.env.PG_USERNAME}:${process.env.PG_PASSWORD}@${process.env.PG_HOST}:${process.env.PG_PORT}/${process.env.PG_DATABASE}`);
-
-try {
-  await sequelize.authenticate();
-  console.log('Connection has been established successfully');
-  await sequelize.close();
-  console.log('Connection has been closed');
-} catch (error) {
-  console.log('Unable to connect to the database', error);
-}
