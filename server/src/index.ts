91e886257c15d64a1533f1d64af26e69f4b63a57
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-07-19 17:35:49 -0600

    Update the proyect structure

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,43 +1,13 @@
-import 'dotenv/config';
-import express from 'express';
-import http from 'http';
-import { Server } from 'socket.io';
+import "dotenv/config";
+import express from "express";
 
 const app = express();
-const server = http.createServer(app);
-const io = new Server(server);
+const port = process.env.PORT || 3000;
 
-app.get('/', (req, res) => {
-  res.send('Hello World');
+app.get("/", (req, res) => {
+  res.send("Hello World");
 });
 
-io.on('connection', (socket) => {
-  console.log('A new user connected');
-
-  socket.on('disconnect', () => {
-    console.log('user disconnected');
-  });
-
-  socket.on('create room', (msg) => {
-    console.log('Room created: ', msg);
-  });
-
-  socket.on('join room', (msg) => {
-    console.log('Room joined: ', msg);
-  });
-
-  socket.on('play card', (msg) => {
-    console.log('Card played: ', msg);
-  });
-
-  socket.on('take card', (msg) => {
-    console.log('A card was taken from the deck: ', msg);
-
-    io.emit('take card', msg);
-  });
-});
-
-server.listen(3000, () => {
-  console.log('Server is running...');
-  console.log('Listening on localhost:3000');
+app.listen(port, () => {
+  console.log(`Server is listening on localhost:${port}`);
 });
