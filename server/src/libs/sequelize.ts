be1c676339fa660f35dfd56df6f59e84cd9dee98
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-07-31 17:32:36 -0600

    Set up models in sequelize

--- server/src/libs/sequelize.ts
+++ server/src/libs/sequelize.ts
@@ -2,10 +2,14 @@ import { Sequelize } from 'sequelize'
 
 import { config } from './../config'
 
+import { setupModels } from './../db/models'
+
 const USER = encodeURIComponent(config.dbUsername)
 const PASSWORD = encodeURIComponent(config.dbPassword)
 const URI = `postgres://${USER}:${PASSWORD}@${config.dbHost}:${config.dbPort}/${config.dbName}`
 
 const sequelize = new Sequelize(URI, { dialect: 'postgres', logging: true })
+
+setupModels(sequelize)
 
 export { sequelize }
