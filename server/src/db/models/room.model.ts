4015bcf1e847f000e082311f3bf084c36a142fcb
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-07-31 17:32:05 -0600

    Change db constants to utils folder

--- server/src/db/models/room.model.ts
+++ server/src/db/models/room.model.ts
@@ -6,7 +6,7 @@ import {
   DataTypes,
 } from 'sequelize'
 
-import { timestampsConfig } from './constants'
+import { timestampsConfig } from '../utils/constants'
 
 export const ROOM_TABLE = 'tbl_room'
 
@@ -20,6 +20,12 @@ export const RoomSchema: ModelAttributes = {
   link: {
     type: DataTypes.UUID,
     allowNull: false,
+  },
+  name: {
+    type: DataTypes.STRING,
+    allowNull: true,
+    // Replace default value with a name auto-generated using faker
+    defaultValue: 'Lorem ipsum',
   },
   status: {
     type: DataTypes.STRING(20),
