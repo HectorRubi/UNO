9f383bd3e1e54835c3b4715a126dfa37a63783c9
Hector Rubi <hector.rubi.garcia@outlook.com>
2023-07-31 18:12:46 -0600

    Add sequelizeErrorHandler middleware to catch all sequelize exceptions

--- server/src/index.ts
+++ server/src/index.ts
@@ -4,7 +4,11 @@ import { Request, Response } from 'express'
 import { config } from './config'
 import { router } from './routes'
 
-import { errorHandler, boomErrorHandler } from './middleware/error.handler'
+import {
+  errorHandler,
+  boomErrorHandler,
+  sequelizeErrorHandler,
+} from './middleware/error.handler'
 
 const app = express()
 const port = config.port
@@ -19,6 +23,7 @@ app.get('/', (req: Request, res: Response) => {
 router(app)
 
 // Personal middleware
+app.use(sequelizeErrorHandler)
 app.use(boomErrorHandler)
 app.use(errorHandler)
 
--- server/src/middleware/error.handler.ts
+++ server/src/middleware/error.handler.ts
@@ -1,5 +1,6 @@
 import { Request, Response, NextFunction } from 'express'
 import { Boom } from '@hapi/boom'
+import { ValidationError } from 'sequelize'
 
 export function errorHandler(err: Error, req: Request, res: Response) {
   res.status(500).json({
@@ -16,6 +17,23 @@ export function boomErrorHandler(
 ) {
   if (err.isBoom) {
     res.status(err.output.statusCode).json(err.output.payload)
+  } else {
+    next(err)
+  }
+}
+
+export function sequelizeErrorHandler(
+  err: ValidationError | Error,
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  if (err instanceof ValidationError) {
+    res.status(409).json({
+      statusCode: 409,
+      message: err.message,
+      errors: err.errors,
+    })
   } else {
     next(err)
   }
